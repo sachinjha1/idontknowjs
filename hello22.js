8ff4e356409fa69079c9d5545ab5f5153fc4d605
Sachin Jha <Sachin.Jha@walmart.com>
2017-06-20 13:31:42 -0500

    Prototypical inheritence explained

--- hello22.js
+++ hello22.js
@@ -12,10 +12,32 @@ var rect1 = new Rectangle(15,30);
 //rect1 is object returned as per 'this' in rectangle function
 //rect1 is an object thats why it will have __proto__ which points to
 //empty object (which is prototype of reactangle function)
-console.log(new rect1.__proto__.constructor(1,3));
+var rect2 = new (rect1.__proto__.constructor)(10,20);
+
+
 //.constructor is property of prototype to show the function (prototype property is alwways on a function)
 
 
 //__proto__ of OBJECT
 //prototype of FUNCTION
-//.constructor (actually a function) of Prototype Object.. 
+//.constructor (actually a function) of Prototype Object..
+
+Rectangle.prototype.area = function(){
+  console.log(this.length * this.width);
+}
+
+rect1.area();
+rect2.area();
+
+
+function Square(l){
+this.length = this.width = l;
+}
+
+//Square.prototype.__proto__ =  Rectangle.prototype;
+/*Object.create creates an empty object that will be in protypical chain of object passed in create()*/
+Square.prototype = Object.create(Rectangle.prototype);
+
+var sq1 = new Square(5);
+
+sq1.area();
