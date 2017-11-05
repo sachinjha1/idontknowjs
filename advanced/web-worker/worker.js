55b75ff8d4c3bf122a9c6104a1bd426335ef0dbb
Sachin Jha <Sachin.Jha@walmart.com>
2017-11-04 21:31:45 -0500

    webworkers: copy bs transfer

--- advanced/web-worker/worker.js
+++ advanced/web-worker/worker.js
@@ -1,3 +1,9 @@
 this.addEventListener("message", function (e) {
-    console.log("Message received ->" + e.data);
+    console.log("Work thread got: " + e.data);
 });
+
+let i = 0;
+
+setInterval(function () {
+    this.postMessage("Counter got: " + i++);
+}.bind(this), 500);
