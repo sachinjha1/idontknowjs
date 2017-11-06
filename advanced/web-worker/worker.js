3a46f81be55e51c6ace29d8999c50f797403e97d
Sachin Jha <Sachin.Jha@walmart.com>
2017-11-05 20:24:50 -0600

    updated web workewrs

--- advanced/web-worker/worker.js
+++ advanced/web-worker/worker.js
@@ -1,9 +1,16 @@
+let msg = null;
+
 this.addEventListener("message", function (e) {
-    console.log("Work thread got: " + e.data);
+    console.log("Work thread got: ", e.data);
+    msg = e.data;
 });
+
+setTimeout(function () {
+    console.log("setTimeout got:", msg);
+}, 1000);
 
 let i = 0;
 
 setInterval(function () {
-    this.postMessage("Counter got: " + i++);
+    //this.postMessage("Counter got: ", i++);
 }.bind(this), 500);
